8422314cf0eecdf8a2ca2b6b24d51e92db51349f
fired <fired@BDF-01-BTTLSTN>
2021-10-06 12:32:49 -0400

    Add project files.

---
+++ Tome Tales Management Panel/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Tome_Tales_Management_Panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainTabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.LootBoxPanel = new System.Windows.Forms.Panel();
+            this.ItemBoxMenuTitle = new System.Windows.Forms.Label();
+            this.BoxSelectionDropdown = new System.Windows.Forms.ComboBox();
+            this.NumOfBoxes = new System.Windows.Forms.NumericUpDown();
+            this.NumBoxesDescriptor = new System.Windows.Forms.Label();
+            this.OpenBoxesExec = new System.Windows.Forms.Button();
+            this.LootBoxOutput = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.MainTabs.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.LootBoxPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumOfBoxes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainTabs
+            // 
+            this.MainTabs.Controls.Add(this.tabPage1);
+            this.MainTabs.Controls.Add(this.tabPage2);
+            this.MainTabs.HotTrack = true;
+            this.MainTabs.Location = new System.Drawing.Point(3, 3);
+            this.MainTabs.Name = "MainTabs";
+            this.MainTabs.SelectedIndex = 0;
+            this.MainTabs.Size = new System.Drawing.Size(794, 444);
+            this.MainTabs.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.LootBoxPanel);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(786, 418);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tome Tales Panel";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(786, 418);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Eterna Panel";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // LootBoxPanel
+            // 
+            this.LootBoxPanel.Controls.Add(this.LootBoxOutput);
+            this.LootBoxPanel.Controls.Add(this.OpenBoxesExec);
+            this.LootBoxPanel.Controls.Add(this.NumBoxesDescriptor);
+            this.LootBoxPanel.Controls.Add(this.NumOfBoxes);
+            this.LootBoxPanel.Controls.Add(this.BoxSelectionDropdown);
+            this.LootBoxPanel.Controls.Add(this.ItemBoxMenuTitle);
+            this.LootBoxPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LootBoxPanel.Location = new System.Drawing.Point(3, 3);
+            this.LootBoxPanel.Name = "LootBoxPanel";
+            this.LootBoxPanel.Size = new System.Drawing.Size(780, 412);
+            this.LootBoxPanel.TabIndex = 0;
+            // 
+            // ItemBoxMenuTitle
+            // 
+            this.ItemBoxMenuTitle.AutoSize = true;
+            this.ItemBoxMenuTitle.Font = new System.Drawing.Font("Exo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ItemBoxMenuTitle.Location = new System.Drawing.Point(48, 0);
+            this.ItemBoxMenuTitle.Name = "ItemBoxMenuTitle";
+            this.ItemBoxMenuTitle.Size = new System.Drawing.Size(140, 22);
+            this.ItemBoxMenuTitle.TabIndex = 0;
+            this.ItemBoxMenuTitle.Text = "Loot Box Controls";
+            // 
+            // BoxSelectionDropdown
+            // 
+            this.BoxSelectionDropdown.BackColor = System.Drawing.SystemColors.Window;
+            this.BoxSelectionDropdown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BoxSelectionDropdown.FormattingEnabled = true;
+            this.BoxSelectionDropdown.Items.AddRange(new object[] {
+            "Gift Box (Anniversary II)",
+            "Gift Box (Spookfest)",
+            "Gift Box (Leon\'s Super Surprise)",
+            "Gift Box (Summer)"});
+            this.BoxSelectionDropdown.Location = new System.Drawing.Point(3, 35);
+            this.BoxSelectionDropdown.Name = "BoxSelectionDropdown";
+            this.BoxSelectionDropdown.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BoxSelectionDropdown.Size = new System.Drawing.Size(239, 21);
+            this.BoxSelectionDropdown.TabIndex = 1;
+            this.BoxSelectionDropdown.Text = "Select Box Type...";
+            this.BoxSelectionDropdown.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // NumOfBoxes
+            // 
+            this.NumOfBoxes.AllowDrop = true;
+            this.NumOfBoxes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NumOfBoxes.Cursor = System.Windows.Forms.Cursors.HSplit;
+            this.NumOfBoxes.Location = new System.Drawing.Point(100, 63);
+            this.NumOfBoxes.Name = "NumOfBoxes";
+            this.NumOfBoxes.Size = new System.Drawing.Size(142, 20);
+            this.NumOfBoxes.TabIndex = 2;
+            this.NumOfBoxes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.NumOfBoxes.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // NumBoxesDescriptor
+            // 
+            this.NumBoxesDescriptor.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.NumBoxesDescriptor.AutoSize = true;
+            this.NumBoxesDescriptor.Location = new System.Drawing.Point(3, 65);
+            this.NumBoxesDescriptor.Name = "NumBoxesDescriptor";
+            this.NumBoxesDescriptor.Size = new System.Drawing.Size(91, 13);
+            this.NumBoxesDescriptor.TabIndex = 3;
+            this.NumBoxesDescriptor.Text = "Number of Boxes:";
+            // 
+            // OpenBoxesExec
+            // 
+            this.OpenBoxesExec.BackColor = System.Drawing.Color.Gainsboro;
+            this.OpenBoxesExec.FlatAppearance.BorderSize = 0;
+            this.OpenBoxesExec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenBoxesExec.Location = new System.Drawing.Point(3, 89);
+            this.OpenBoxesExec.Name = "OpenBoxesExec";
+            this.OpenBoxesExec.Size = new System.Drawing.Size(239, 23);
+            this.OpenBoxesExec.TabIndex = 4;
+            this.OpenBoxesExec.Text = "Commence Opening";
+            this.OpenBoxesExec.UseVisualStyleBackColor = false;
+            this.OpenBoxesExec.Click += new System.EventHandler(this.OpenBoxesExec_Click);
+            // 
+            // LootBoxOutput
+            // 
+            this.LootBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.LootBoxOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LootBoxOutput.Location = new System.Drawing.Point(3, 118);
+            this.LootBoxOutput.Multiline = true;
+            this.LootBoxOutput.Name = "LootBoxOutput";
+            this.LootBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LootBoxOutput.Size = new System.Drawing.Size(239, 291);
+            this.LootBoxOutput.TabIndex = 5;
+            this.LootBoxOutput.Text = "Sample Text";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainTabs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Eterna Control Center | Iteration 85 | v0.16";
+            this.MainTabs.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.LootBoxPanel.ResumeLayout(false);
+            this.LootBoxPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumOfBoxes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl MainTabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel LootBoxPanel;
+        private System.Windows.Forms.Label ItemBoxMenuTitle;
+        private System.Windows.Forms.ComboBox BoxSelectionDropdown;
+        private System.Windows.Forms.Label NumBoxesDescriptor;
+        private System.Windows.Forms.NumericUpDown NumOfBoxes;
+        private System.Windows.Forms.TextBox LootBoxOutput;
+        private System.Windows.Forms.Button OpenBoxesExec;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Tome Tales Management Panel/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tome_Tales_Management_Panel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenBoxesExec_Click(object sender, EventArgs e)
+        {
+            LootBoxOutput.Text = "";
+            // Loot Box management system here, change things as needed.
+            string selBox = BoxSelectionDropdown.Text;
+            int rolls = ((int)NumOfBoxes.Value);
+            LootBoxOutput.Text = ("Opening " +rolls +" " +selBox +Environment.NewLine);
+
+            // BOX TYPES | Add, remove and do whatever right here.
+            string[] SummerGiftBox = { "Slime Egg :sparkles:", "5,000 BP", "Bamboo Rod", "Cooked Makiyoa", "Blue Pearl", "Golden Shell", "Red Coral" };
+            string[] SpookfestBox = { "5 Krackle Berriy", "5 Glum Berry", "Radile Pop", "Krackle Drops", "3 Ether", "Minhtment", "5 Radile Berry", "Bitter Soul", "Sizzle Pop", "Freezle Gum", "Haunt Attack :warning:", "Sour Soul", "5 Freezle Berry", "Glum Gum", "5 Sizzle Berry", "6,500 BP!", "Nymare Ambush :warning:", "Sweet Soul", "5 Minht Berry", "3 Health Potion" };
+            string[] FoxDeluxeSurpriseBox = { "Companion Card: Bolt", "Velgha", "Golden Egg :sparkles:", "Super Kaboom", "Tokyo Key: Rainbow", "Slime Egg :sparkles:", "3 Leonade Deluxe", "Nodakime Nageku", "Job Bonus Key: Synergist Knight", "Companion Card: Aka", "50k BP!!!", "10 Golden Key!", "3 Companion Card: Leon", "Freeze Blast", "Nova Dominus", "Job Bonus Key: Guardian", "Rainbow Egg :sparkles:", "5 Diamond Key", "Foxxo Mode! :fox:", "Companion Card: Weiss", "Bouncy Time!", "Arma Gem", "Rappy Fever!", "Haunt Assault :warning:", "3 Job Token", "Nymare Onslaught :warning:", "Companion Card: Silene", "Gambler Pass", "Dread King's Arrival :warning:", "Double Double Mode!", "Altar Spin", "Slime Rampage", "25k BP!!!", "Bandit Attack! :warning:", "Jackpot!!!", "5 Job Token", "Job Key: Astrologian", "Rappy Egg! :sparkles:", "Companion Card: Lea", "Companion Card: Kumiki", "25 Asterisk Shards", "Radiant Virtue"};
+            string[] AnniversaryBox = { "5 Slumber Orb", "5 Totter Orb", "10 Oracle Berry", "Diamond Key", "5 Silver Key", "5 Golden Key", "Old Scarf", "5 Sandy Orb", "5 Rainy Orb", "5 Sunny Orb", "20k BP!!!", "Golden Egg!!! :sparkles:", "25 Asterisk Shards", "15k BP!!", "Contract", "3 Revive All Orb" };
+
+            for (int i = 0; i < rolls; i++)
+            {
+
+                Random BoxRnd = new Random();
+                if (selBox.Equals("Gift Box (Summer)"))
+                {
+                    System.Threading.Thread.Sleep(15);
+                    LootBoxOutput.Text = LootBoxOutput.Text + Environment.NewLine + SummerGiftBox[BoxRnd.Next(SummerGiftBox.Length)];
+                }
+                else if (selBox.Equals("Gift Box (Spookfest)"))
+                {
+                    System.Threading.Thread.Sleep(15);
+                    LootBoxOutput.Text = LootBoxOutput.Text + Environment.NewLine + SpookfestBox[BoxRnd.Next(SpookfestBox.Length)];
+                }
+                else if (selBox.Equals("Gift Box (Leon's Super Surprise)"))
+                {
+                    System.Threading.Thread.Sleep(15);
+                    LootBoxOutput.Text = LootBoxOutput.Text + Environment.NewLine + FoxDeluxeSurpriseBox[BoxRnd.Next(FoxDeluxeSurpriseBox.Length)];
+                }
+                else if (selBox.Equals("Gift Box (Anniversary II)"))
+                {
+                    System.Threading.Thread.Sleep(15);
+                    LootBoxOutput.Text = LootBoxOutput.Text + Environment.NewLine + AnniversaryBox[BoxRnd.Next(AnniversaryBox.Length)];
+                }
+            }
+        }
+    }
+}
